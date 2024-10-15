50f1c12aeb5a06b32fcb1392a4ba2f79e7d4498f
Nelson Ramirez <xhyskoax@gmail.com>
2024-10-14 21:07:46 -0300

    uso de contextos básico

--- ejemplo2/ejemplo2/views.py
+++ ejemplo2/ejemplo2/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    contexto = {}
+    contexto = {
+        "titulo": "Bienvenidos a nuestra web",
+        
+    }
     return render(request, 'home.html', contexto)
 
 def contacto(request):
-    contexto = {}
+    contexto = {
+        "titulo": "Gracias por querer contactarte con nosotros"
+    }
     return render(request, 'contacto.html', contexto)
 
 def reclamos(request):
-    contexto = {}
+    contexto = {
+        "titulo": "Deja tu reclamo en el siguiente Formulario"
+    }
     return render(request, 'reclamos.html', contexto)
